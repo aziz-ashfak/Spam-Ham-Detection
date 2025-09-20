3e7d46782e951e28d56e5395d3a54e1b5945891e
AzizAshfak <azizashfak@gmail.com>
2025-09-20 16:23:33 +0600

    update

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ import numpy as np
 import os
 import sys
 import nltk
-
 nltk.download('punkt')
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
+nltk.download('punkt_tab')
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),'../')))
 from src.pipeline.predict_pipeline import make_prediction
 
 app = Flask(__name__)
@@ -38,6 +38,9 @@ def predict():
     except Exception as e:
         print("Prediction Error:", e, file=sys.stderr)  # log error in console
         return jsonify({"success": False, "error": str(e)}), 500
+if __name__ == "__main__":
+    print("Starting Flask app...")
+    app.run(debug=True, host='127.0.0.1', port=5000)
 
 
 
